42f236db76f8de20f3f45fabfdc5f96a13016350
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2019-04-09 16:09:19 -0300

    Added board creation logic

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="row in rowsAmount" :key="row" class="row">
+      <SquareBoard v-for="column in columnsAmount" :key="column" :x="row" :y="column"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import SquareBoard from "./SquareBoard.vue";
+
+export default {
+  name: "GameBoard",
+  props: {
+    rowsAmount: {
+      type: Number,
+      default: 6
+    },
+    columnsAmount: {
+      type: Number,
+      default: 6
+    }
+  },
+  data() {
+    return {};
+  },
+  components: {
+    SquareBoard
+  }
+};
+</script>
---
+++ src/components/SquareBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div @click="onClick" class="square col-md-1"></div>
+</template>
+
+<script>
+export default {
+  name: "SquareBoard",
+  props: {
+    x: Number,
+    y: Number
+  },
+  methods: {
+    onClick() {
+      const x = this.x;
+      const y = this.y;
+      console.log(`X: ${x} | Y: ${y}`);
+    }
+  }
+};
+</script>
+
